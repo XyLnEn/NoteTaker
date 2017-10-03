7440362878ed06019b722a291ba2687ccd0d3fd0
umahey <ugomahey@hotmail.fr>
2017-10-03 09:32:47 +0200

    Ajout des controlleurs, des services et modification de BootStrap (la fonction lancée au démarrage pour tester les différents services et controllers).

--- server/grails-app/init/notetaker/BootStrap.groovy
+++ server/grails-app/init/notetaker/BootStrap.groovy
@@ -1,8 +1,25 @@
 package notetaker
 
+import org.springframework.beans.factory.annotation.Autowired
+
+
 class BootStrap {
 
+    def clientService
+    def missionService
+    def personneService
+    def technosService
+    def savoirEtreService
     def init = { servletContext ->
+        Personne p = personneService.creerPersonne("NomTest1","PrenomTest1","FonctionTest1")
+        clientService.creerClient("testNom2","testLieu2")
+        clientService.creerClient("testNom3","testLieu3")
+        Technos t = technosService.creerTechnos("testNom1",1)
+        Technos t2 = technosService.creerTechnos("testNom2",2)
+        missionService.creerMission("testIntitule1","testLieu1",p,Arrays.asList(t,t2))
+        savoirEtreService.creerSavoirEtre("testQualite1")
+        def m = missionService.rechercherMissionParTechnos([t])
+
     }
     def destroy = {
     }
